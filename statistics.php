c42dc3f88ea21703c3ad0a2ecb8025ac5db34b5b
Alfie <alfiecollins44@gmail.com>
2024-02-14 17:45:04 +0000

    statistics page

---
+++ statistics.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Statstics</title>
+</head>
+<body>
+<?php include('templates/header.php'); ?>
+    <h1>Blog Spot Statistics</h1>
+    <h4>Just for your curiosity</h4>
+    <h6>Total Users</h6>
+    <h6>Total Words</h6>
+    <h6>Total Blogs</h6>
+    <h6>Most Popular Topic</h6>
+    <?php include('templates/footer.php'); ?>
+</body>
+</html>
