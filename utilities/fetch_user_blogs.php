135e6017fe02e6d4a52ca56857f1feb7a6721b4b
Alfie <alfiecollins44@gmail.com>
2024-02-24 17:30:25 +0000

    change to fetch user blogs fields

--- utilities/fetch_user_blogs.php
+++ utilities/fetch_user_blogs.php
@@ -21,11 +21,11 @@ if (isset($_GET["id"])) {
     }
 
     // Fetch blogs for the user with search functionality
-    $stmt_blogs = $mysqli->prepare("SELECT blogs.title, blogs.date, blogs.content, blogs.id, blogs.topic, user.user_id, user.name as author_name
-        FROM blogs
-        INNER JOIN user ON blogs.user_id = user.user_id
-        WHERE blogs.user_id = ? AND (blogs.title LIKE ? OR blogs.topic LIKE ?)
-        ORDER BY blogs.date DESC, blogs.id DESC");
+    $stmt_blogs = $mysqli->prepare("SELECT blogs.title, blogs.date, blogs.last_updated, blogs.content, blogs.id, blogs.topic, user.user_id, user.name as author_name
+    FROM blogs
+    INNER JOIN user ON blogs.user_id = user.user_id
+    WHERE blogs.user_id = ? AND (blogs.title LIKE ? OR blogs.topic LIKE ?) AND blogs.is_draft = 0
+    ORDER BY COALESCE(blogs.last_updated, blogs.date) DESC, blogs.id DESC");
     $likeParam = "%$searchTerm%";
     $stmt_blogs->bind_param("iss", $user_id, $likeParam, $likeParam);
     $stmt_blogs->execute();
@@ -33,7 +33,6 @@ if (isset($_GET["id"])) {
 
     // Fetch the resulting rows as an array
     $blogs = $result_blogs->fetch_all(MYSQLI_ASSOC);
-
 
     function calculateWordCount($content) {
         // Count words by counting spaces
@@ -45,17 +44,18 @@ if (isset($_GET["id"])) {
     <div class="row">
         <a href="drafts.php?id=<?php echo $loggedInUserId; ?>" class="left underline">See Drafts</a>
         <?php foreach($blogs as $blog): ?>
-            <div class="col s12" style="border: 1px solid grey;" >
-                <a href="view.php?id=<?php echo $blog['id']; ?>" class="center grey-text text-darken-2">
-                    <div class="card-content">
-                        <h6 style="font-weight: bold"><?php echo htmlspecialchars($blog['title']); ?></h6>
-                        <p style="font-size: smaller">Topic: <?php echo htmlspecialchars($blog['topic']); ?></p>
-                        <p style="font-size: smaller">Word Count: <?php echo calculateWordCount($blog['content']); ?></p>
-                        <p style="font-size: smaller">Created on <?php echo date('d-m-Y', strtotime($blog['date'])); ?></p>
-                    </div>
-                </a>
-            </div>
-        <?php endforeach; ?>
+                <div class="col s12 profile-card" style="border: 1px solid grey;" >
+                    <a href="view.php?id=<?php echo $blog['id']; ?>" class="center grey-text text-darken-2">
+                        <div class="card-content">
+                            <h6 style="font-weight: bold"><?php echo htmlspecialchars($blog['title']); ?></h6>
+                            <p style="font-size: smaller">Topic: <?php echo htmlspecialchars($blog['topic']); ?></p>
+                            <p style="font-size: smaller">Word Count: <?php echo calculateWordCount($blog['content']); ?></p>
+                            <p style="font-size: smaller">Created On: <?php echo date('d-m-Y', strtotime($blog['date'])); ?></p>
+                            <p style="font-size: smaller">Last Updated: <?php echo date('d M Y H:i:s', strtotime($blog['last_updated'])); ?></p>
+                        </div>
+                    </a>
+                </div>
+            <?php endforeach; ?>
     </div>
     <?php
 
