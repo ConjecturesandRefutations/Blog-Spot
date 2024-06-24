e0b0d7c235f79137586c4b2cc11f489a4a8273c1
Alfie <alfiecollins44@gmail.com>
2024-06-24 19:43:09 +0100

    removing unneccessary session start

--- all-users.php
+++ all-users.php
@@ -1,11 +1,6 @@
 <?php
 include('config/db_connect.php');
 
-if (session_status() == PHP_SESSION_NONE) {
-    // Start the session only if it's not already started
-    session_start();
-}
-
 // Initialize the search variable
 $search = '';
 
