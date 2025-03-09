11c2eca310eafb2594c58ededca1d066b94ffcd3
Alfie <alfiecollins44@gmail.com>
2025-03-09 11:59:12 +0000

    more style changes

--- all-users.php
+++ all-users.php
@@ -63,30 +63,34 @@ function calculateWordCount($content) {
 <!-- Add the missing container -->
 <div class="container">
     <div class="row" id="user-list">
-    <?php foreach ($users as $profileUser) : ?>
-      <div class="col s12 user" style="border: 1px solid grey; <?php if (isset($_SESSION['user_id']) && $profileUser['user_id'] == $_SESSION['user_id']) echo 'background-color: #dddddd;'; ?>">
-        <a href="profile.php?id=<?php echo $profileUser['user_id']; ?>" class="card-content grey-text text-darken-2 user-card">
-            <div class="img">
-            <img src="<?php echo (!empty($profileUser['profile_image'])) ? $profileUser['profile_image'] : 'images/defaultProfile.jpg'; ?>" alt="Profile Image" class="circle profile-all">
+        <?php foreach ($users as $profileUser) : ?>
+            <div class="col s12 m6 l4">
+                <div class="card <?php if (isset($_SESSION['user_id']) && $profileUser['user_id'] == $_SESSION['user_id']) echo 'grey lighten-3'; ?>">
+                    <div class="card-content">
+                        <div class="center-align">
+                            <img src="<?php echo (!empty($profileUser['profile_image'])) ? $profileUser['profile_image'] : 'images/defaultProfile.jpg'; ?>" alt="Profile Image" class="circle responsive-img" style="width: 80px; height: 80px;">
+                        </div>
+                        <div class="user-info center-align">
+                            <h6 class="black-text" style="font-weight: bold;"> <?php echo htmlspecialchars($profileUser['name']); ?> </h6>
+                            <p class="grey-text text-darken-2">Total Blogs: <?php echo $profileUser['numBlogs']; ?></p>
+                            
+                            <?php 
+                            $totalWords = 0;
+                            $blogsResult = mysqli_query($conn, "SELECT content FROM blogs WHERE user_id = {$profileUser['user_id']} AND is_draft = 0");
+                            while ($blog = mysqli_fetch_assoc($blogsResult)) {
+                                $totalWords += calculateWordCount($blog['content']);
+                            }
+                            mysqli_free_result($blogsResult);
+                            ?>
+                            <p class="grey-text text-darken-2">Total Words: <?php echo $totalWords; ?></p>
+                            <p class="grey-text text-darken-2 favourite-topic">Favourite Topic: <?php echo htmlspecialchars($profileUser['favoriteTopic']); ?></p>
+                        </div>
+                    </div>
+                    <div class="card-action center-align">
+                        <a href="profile.php?id=<?php echo $profileUser['user_id']; ?>" class="btn blue lighten-1 waves-effect waves-light">View Profile</a>
+                    </div>
+                </div>
             </div>
-            <div class="user-info">
-            <h6 style="font-weight: bold"><?php echo htmlspecialchars($profileUser['name']); ?></h6>
-            <p style="font-size: smaller">Total Blogs: <?php echo $profileUser['numBlogs']; ?></p>
-            <!-- Calculate and display total words -->
-            <?php 
-            $totalWords = 0;
-            // Fetch blogs for the user
-            $blogsResult = mysqli_query($conn, "SELECT content FROM blogs WHERE user_id = {$profileUser['user_id']} AND is_draft = 0");
-            while ($blog = mysqli_fetch_assoc($blogsResult)) {
-                $totalWords += calculateWordCount($blog['content']);
-            }
-            mysqli_free_result($blogsResult);
-            ?>
-            <p style="font-size: smaller">Total Words: <?php echo $totalWords; ?></p>
-            <p style="font-size: smaller">Favourite Topic: <?php echo htmlspecialchars($profileUser['favoriteTopic']); ?></p>
-            </div>
-        </a>
-    </div>
 <?php endforeach; ?>
 
     </div>
@@ -97,7 +101,6 @@ function calculateWordCount($content) {
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
 $(document).ready(function() {
-    // Function to fetch and display users in real-time
     function fetchUsers(search) {
         $.ajax({
             type: "GET",
@@ -108,22 +111,27 @@ $(document).ready(function() {
                 var userList = $("#user-list");
                 userList.empty();
 
-                // Iterate through fetched users and append to the list
                 users.forEach(function(profileUser) {
-                    var userCard = `<div class="col s12 user" data-user-id="${profileUser.user_id}" style="border: 1px solid grey;">
-                                <a href="profile.php?id=${profileUser.user_id}" class="card-content center grey-text text-darken-2 user-card">
-                                <div class="img">
-                                    <img src="${(profileUser.profile_image) ? profileUser.profile_image : 'images/defaultProfile.jpg'}" alt="Profile Image" class="circle profile-all">
+                    var userCard = `
+                        <div class="col s12 m6 l4 user" data-user-id="${profileUser.user_id}">
+                            <div class="card ${profileUser.isCurrentUser ? 'grey lighten-3' : ''}">
+                                <div class="card-content">
+                                    <div class="center-align">
+                                        <img src="${profileUser.profile_image ? profileUser.profile_image : 'images/defaultProfile.jpg'}" 
+                                             alt="Profile Image" class="circle responsive-img profile-all" style="width: 80px; height: 80px;">
+                                    </div>
+                                    <div class="user-info center-align">
+                                        <h6 class="black-text user-name" style="font-weight: bold;">${profileUser.name}</h6>
+                                        <p class="grey-text text-darken-2 num-blogs">Total Blogs: ${profileUser.numBlogs}</p>
+                                        <p class="grey-text text-darken-2 total-words">Total Words: ${profileUser.totalWords}</p>
+                                        <p class="grey-text text-darken-2 favourite-topic">Favourite Topic: ${profileUser.favoriteTopic}</p>
+                                    </div>
                                 </div>
-                                <div class="user-info">
-                                    <h6 class="user-name" style="font-weight: bold">${profileUser.name}</h6>
-                                    <p class="num-blogs" style="font-size: smaller">Total Blogs: ${profileUser.numBlogs}</p>
-                                    <!-- Calculate and display total words -->
-                                    <p class="total-words" style="font-size: smaller">Total Words: ${profileUser.totalWords}</p>
-                                    <p class="favorite-topic" style="font-size: smaller">Favorite Topic: ${profileUser.favoriteTopic}</p>
+                                <div class="card-action center-align">
+                                    <a href="profile.php?id=${profileUser.user_id}" class="btn blue lighten-1 waves-effect waves-light">View Profile</a>
                                 </div>
-                                </a>
-                            </div>`;
+                            </div>
+                        </div>`;
                     userList.append(userCard);
                 });
             }
